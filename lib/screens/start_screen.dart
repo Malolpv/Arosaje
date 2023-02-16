ddb31bfd54a1073cba9bec8c9726a222fd345432
Mathis <mathis.dore@epsi.fr>
2023-02-16 15:34:23 +0100

    ui/ux modification for main and start

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'sign_in.dart';
 import 'sign_up.dart';
@@ -8,29 +7,47 @@ class StartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome to MyApp'),
+        title: const Text('Arosaje'),
       ),
-      body: Center(
+      body: 
+      Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
+            const Text(
+              'Arosaje',
+              style: TextStyle(
+                fontSize: 24, 
+                fontWeight: FontWeight.bold, 
+                color: Colors.green,
+                ),
+            ),
+            SizedBox(
+              height: 50,
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => SignInPage()),
                 );
-              },
-              child: Text('Sign In'),
+                },
+                child: const Text('Sign In'),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
+            const SizedBox(height: 16),
+            SizedBox(
+              height: 50,
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => SignUpPage()),
                 );
-              },
-              child: Text('Sign Up'),
+                },
+                child: const Text('Sign Up'),
+              ),
             ),
           ],
         ),
