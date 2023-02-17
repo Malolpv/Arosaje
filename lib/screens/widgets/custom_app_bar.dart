5ed74213c9b1951e3fa3c8bf90b1beb60f004d91
Malo <mlepavec.ledantec@gmail.com>
2023-02-17 13:47:12 +0000

    update on Custom App Bar

--- lib/screens/widgets/custom_app_bar.dart
+++ lib/screens/widgets/custom_app_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
   final String title;
 
   const CustomAppBar({super.key, required this.title});
@@ -9,18 +9,44 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Size get preferredSize => const Size.fromHeight(60.0);
 
   @override
+  State<StatefulWidget> createState() => _CustomAppBarState();
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  Future openLogOutDialog(BuildContext context) async => showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+            title: const Text("Déconnexion"),
+            content: const Text("Êtes vous sûr de vouloir vous déconnecter ?"),
+            actions: [
+              TextButton(
+                  onPressed: (() => Navigator.of(context).pop()),
+                  child: const Text("Cancel")),
+              TextButton(
+                  onPressed: () async {
+                    //TODO SETUP DECONNECTION
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("Ok"))
+            ],
+          ));
+
+  @override
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.green,
       title: Text(
-        title,
+        widget.title,
       ),
       actions: [
         IconButton(
+          //TODO IMPLEMENTER ICONE ACCOUNT
           onPressed: () => throw Exception(),
           icon: Icon(Icons.account_circle),
         ),
-        IconButton(onPressed: () => throw Exception(), icon: Icon(Icons.logout))
+        IconButton(
+            onPressed: () => openLogOutDialog(context),
+            icon: Icon(Icons.logout))
       ],
       centerTitle: true,
     );
