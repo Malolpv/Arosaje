a9148e8d170a5b48302320b724210eb9e878d5f2
Malolpv <77495604+Malolpv@users.noreply.github.com>
2023-02-21 12:42:19 +0100

    Create plant_detail_view_model.dart

---
+++ lib/viewmodels/plant_detail_view_model.dart
@@ -0,0 +1,21 @@
+class PlantDetailViewModel extends BaseViewModel{
+  
+  //attributes 
+  Plant? _plant;
+  Plant? get plant => _plant;
+  
+  //Constructors
+  PlantDetailViewModel(this._plant);
+  
+  //utils
+  
+  
+  //errors
+  
+  
+  //methods
+  
+  void fetchPictures(){
+    //TODO retrieve pictures by plant ID
+  }
+}
