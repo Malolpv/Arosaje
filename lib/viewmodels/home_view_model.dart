a9115bd2950ab5e736e1cbc2217519e14107e319
Malolpv <77495604+Malolpv@users.noreply.github.com>
2023-02-21 12:41:43 +0100

    Create home_view_model.dart

---
+++ lib/viewmodels/home_view_model.dart
@@ -0,0 +1,36 @@
+class HomeViewModel extends BaseViewModel{
+  //TODO ADD SERVICE CALL TO RETRIEVE DATA
+   
+  
+  //attributes
+  List<Plant> _plantList = [];
+  List<Plant> get plantList => _plantList;
+  
+  //errors
+  
+  
+  //methods
+  
+  void plantTaped(int index){
+    //TODO redirect to the detail page
+    //Navigator.push(context,MaterialPageRoute(builder: (context) => const PlantDetailView(PlantDetailViewModel(_plantList[index])))
+    
+  }
+  
+  void refreshPlantList() async {
+    _isLoading = true;
+    //notifyListeners();
+    
+    fetchPlantList();
+    
+    _isLoading = false;
+    //notifyListeners();
+  }
+  
+  void fetchPlantList() async {
+    //TODO EDIT WITH AN API CALL
+    _plantList = [];
+  }
+  
+  
+}
