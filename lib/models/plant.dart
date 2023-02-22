70701bdcead75a513a5be5791e882802a7abc2ab
Malo <mlepavec.ledantec@gmail.com>
2023-02-22 23:34:46 +0000

    update our use case

--- lib/models/plant.dart
+++ lib/models/plant.dart
@@ -1,1 +1,36 @@
-class Plant {}
+import 'package:arosaje/models/picture.dart';
+import 'package:flutter/material.dart';
+
+class Plant {
+  //attributes
+  int _id;
+  String _name;
+  DateTime _creationDate;
+  List<Picture> _pictures;
+  Picture _mainPicture;
+
+  // getters && setters
+  int get id => _id;
+  String get name => _name;
+  DateTime get creationDate => _creationDate;
+  List<Picture> get pictures => _pictures;
+  Picture get mainPicture => _mainPicture;
+
+  set name(String value) {
+    _name = value;
+  }
+
+  set creationDate(DateTime value) {
+    _creationDate = value;
+  }
+
+  set mainPicture(Picture value) {
+    _mainPicture = value;
+  }
+
+  //Constructors
+  Plant(this._id, this._name, this._creationDate, this._mainPicture,
+      this._pictures);
+
+  //methods
+}
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,1 +1,7 @@
-class User {}
+class User {
+  final int id;
+  final String email;
+  final String pass;
+
+  const User(this.id, this.email, this.pass);
+}
