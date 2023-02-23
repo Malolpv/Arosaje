da164948577ef1f07aecb52469f6e1936b2f6c61
Malo <mlepavec.ledantec@gmail.com>
2023-02-23 22:53:06 +0000

    routing setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:arosaje/navigation/navigation_delegate.dart';
+import 'package:arosaje/navigation/navigation_route_parser.dart';
 import 'package:flutter/material.dart';
 import 'screens/start_screen.dart';
 
@@ -5,18 +7,26 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+
+  final NavigationDelegate _navigationDelegate = NavigationDelegate();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Arosaje',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: StartScreen(),
+      routeInformationParser: NavigationRouteParser(),
+      routerDelegate: _navigationDelegate,
     );
   }
 }
